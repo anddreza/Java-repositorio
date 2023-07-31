3e97c722518c59918d4f2e3153fcdff9a6d826a8
Andreza <dedezasilva@live.com>
2023-07-31 15:09:52 -0300

    Sobre Enums

--- src/JavaModulo2/Aula53/DiaSemana.java
+++ src/JavaModulo2/Aula53/DiaSemana.java
@@ -1,2 +1,13 @@
-package JavaModulo2.Aula53;public enum DiaSemana {
+package JavaModulo2.Aula53;
+
+public enum DiaSemana {
+    SEGUNDA(1), TERCA(2), QUARTA(3), QUINTA(4), SEXTA(5), SABADO(6), DOMINGO(7);
+    private int valor;
+    DiaSemana(int valor){
+        this.valor = valor;
+    }
+
+    public int getValor(){
+        return this.valor;
+    }
 }
